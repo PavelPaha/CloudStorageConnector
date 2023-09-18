fcd222c5ccf942183899420feaca7efabb528cfa
Pavel Vasiliev <vasilievpavel4@gmail.com>
2023-09-18 16:56:53 +0500

    Сохранение папок из гугл драйв

--- fileDownloader.py
+++ fileDownloader.py
@@ -17,7 +17,10 @@ class Downloader:
 
     mime_google_type_to_file_application = {
         "application/vnd.google-apps.spreadsheet": "xlsx",
-        "application/vnd.google-apps.document": "docx"
+        "application/vnd.google-apps.document": "docx",
+        "image/jpeg": "jpeg",
+        "application/pdf": "pdf",
+        "application/vnd.google-apps.folder": ""
     }
 
     mime_google_type_to_default_mime_type = {
@@ -42,24 +45,10 @@ class Downloader:
 
         self.service = build('drive', 'v3', credentials=creds)
 
-    def download_file_data(self, real_file_id):
-        """Downloads a file
-        Args:
-            real_file_id: ID of the file to download
-        Returns : IO object with location.
-
-        Load pre-authorized user credentials from the environment.
-        for guides on implementing OAuth2 for the application.
-        """
-
+    def download_file_data(self, file_info):
         try:
-            file_id = real_file_id
-
-            # pylint: disable=maybe-no-member
-            file_info = self.service.files().get(fileId=file_id).execute()
+            file_id = file_info["id"]
             mimeType = file_info['mimeType']
-            request = None
-
             if self.mime_google_type_to_default_mime_type.__contains__(mimeType):
                 request = self.service.files().export(fileId=file_id,
                                                       mimeType=self.mime_google_type_to_default_mime_type[mimeType])
@@ -68,18 +57,41 @@ class Downloader:
 
             # request = self.service.files().export_media(fileId=file_id, mimeType=mimeType)
 
-            file = io.BytesIO()
-            downloader = MediaIoBaseDownload(file, request)
-            done = False
-            while done is False:
-                status, done = downloader.next_chunk()
-                print(F'Download {int(status.progress() * 100)}.')
+            file = self.send_request(request)
 
         except HttpError as error:
             print(F'An error occurred: {error}')
             file = None
 
-        return file.getvalue(), file_info
+        return file.getvalue()
+
+    def send_request(self, request):
+        file = io.BytesIO()
+        downloader = MediaIoBaseDownload(file, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print(F'Download {int(status.progress() * 100)}.')
+        return file
+
+    def download_folder(self, folder_info):
+        folder_name = folder_info["name"]
+        folder_id = folder_info["id"]
+
+        if not os.path.exists(folder_name):
+            os.mkdir(folder_name)
+
+        results = self.service.files().list(
+            q=f"'{folder_id}' in parents",
+            pageSize=10, fields="nextPageToken, files(id, name)",
+            pageToken=None).execute()
+        inner_files = results.get('files', [])
+        for file in inner_files:
+            file_id = file["id"]
+            file_info = self.service.files().get(fileId=file_id).execute()
+            file = self.download_file_data(file_info)
+
+            self.save_file_in_directory(file, file_info, folder_name)
 
     def get_file_names(self, count):
         results = self.service.files().list(
@@ -90,13 +102,31 @@ class Downloader:
             print('No files found.')
             return
         print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
+        return [item['id'] for item in items]
 
-    def save_file(self, file_id, path='downloads'):
-        data = self.download_file_data(real_file_id=file_id)
-        file_name = f"{str(data[1]['name'])}"
-        file_extension = self.mime_google_type_to_file_application[data[1]['mimeType']]
-        file_path = os.path.join(path, f"{file_name}.{file_extension}")
+    def is_folder(self, file_info):
+        mimeType = file_info['mimeType']
+        return mimeType == 'application/vnd.google-apps.folder'
+
+    def save(self, file_id, path='downloads'):
+        print(f"Началась загрузка файла с id = {file_id}")
+        file_info = self.service.files().get(fileId=file_id).execute()
+
+        if self.is_folder(file_info):
+            self.download_folder(file_info)
+            return
+
+        data = self.download_file_data(file_info).getvalue()
+        file_info = self.service.files().get(fileId=file_id).execute()
+        file = data
+
+        self.save_file_in_directory(file, file_info, path)
+
+    def save_file_in_directory(self, file, file_info, path):
+        file_id = file_info["id"]
+        file_name = f"{str(file_info['name'])}"
+        file_extension = self.mime_google_type_to_file_application[file_info['mimeType']]
+        file_path = f"{path}/{file_name}.{file_extension}"
         with open(file_path, 'wb+') as f:
-            f.write(data[0])
+            f.write(file)
+        print(f"Файл с id = {file_id} загружен в {file_path}")
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import fileDownloader
 
 if __name__ == '__main__':
     downloader = fileDownloader.Downloader()
-    file_id = '1-74-JwelSUzVxUCUblThfs9AtuFr_QdFZ1RFrbluxW0'
-    downloader.save_file(file_id)
-    downloader.get_file_names(100)
+    file_id = '1RyJ9mlD_QFyfvgwrVTtqg5W3niXWV8WZ'
+    # ids = downloader.get_file_names(40)
+    # for id in ids:
+    #     downloader.save_file(id)
+    downloader.save('1pNF7i6pwVeiJv3Bwc_hiw0w0JVsXNJAr')
